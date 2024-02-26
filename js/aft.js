c47e7b999d0a1a7e3a3d92de962476c297487c7a
aftab0786k <aftabmiya579@gmail.com>
2024-02-26 10:05:01 +0530

    js file

---
+++ js/aft.js
@@ -0,0 +1,13 @@
+
+const main = document.getElementById("main-heading");
+ console.log(main);
+ main.textContent ="Fruit World";
+ main.style.color ="orange";
+  const header =document.getElementById("header");
+   console.log(header);
+  header.style.backgroundColor ="green";
+  header.style.border = "orange";
+  const basket = document.getElementById("basket-heading");
+  basket.style.color ="green";
+  const abc = document.getElementById("thanks");
+  abc.innerHTML ="<p>Please visit us again</p>";
